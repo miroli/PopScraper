ee4545a7c290463f6454c38c446f59b8c22f633b
vienno <robin.linderborg@gmail.com>
2014-10-19 18:20:22 +0200

    new api, tested for year_start=2006

--- PopScraper.py
+++ PopScraper.py
@@ -4,148 +4,108 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pprint
+import re
+import math
 
 class PopScraper(object):
-	URL = 'http://www.popfakta.se/sv/avancerad-sokning/'
-	COOKIE_NAME = 'ASP.NET_SessionId'
+    URL = 'http://www.popfakta.se/sv/avancerad-sokning/'
+    COOKIE_NAME = 'ASP.NET_SessionId'
+    FAKE_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36'
 
-	def __init__(self, artist='', year_start='--'):
-		self._init_session()
-		self._set_payload(year_start)
-		self._fetch_results()
+    def __init__(self, filename='results.csv', artist='', year_start='--', year_end='--'):
+        self.filename = filename
+        self.artist = artist
+        self.year_start = year_start
+        self.year_end = year_end
 
-	def _init_session(self):
-		self.session = requests.Session()
-		resp = self.session.get(PopScraper.URL)
-		self._set_validation(resp)
+        self._init_session()
+        # self._fetch_results()
 
-	def _set_validation(self, response):
-		validation = {}
-		soup = bs(response.text)
-		validators = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION']
+    def _init_session(self):
+        self.session = requests.Session()
+        resp = self.session.get(PopScraper.URL)
+        self._set_validation(resp)
 
-		for validator in validators:
-			input_element = soup.find('input', { 'id': validator })
-			validation[validator] = input_element['value']
+    def _set_validation(self, response):
+        validation = {}
+        soup = bs(response.text)
+        validators = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION']
 
-		self.validation = validation
+        for validator in validators:
+            input_element = soup.find('input', { 'id': validator })
+            validation[validator] = input_element['value']
 
-	def _set_payload(self, year_start='--'):
-		self.payload = {
-			'__EVENTTARGET': '',
-			'__EVENTARGUMENT': '',
-			'__LASTFOCUS': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchAlbum': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchArtist': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchSkivbolag': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchDistributor': '',
-			'ctl00$ContentPlaceHolder1$ddlFormat': '0',
-			'ctl00$ContentPlaceHolder1$ddlYearStart': year_start,
-			'ctl00$ContentPlaceHolder1$tbxSearchNo': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchTrack': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchStudio': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchForlag': '',
-			'ctl00$ContentPlaceHolder1$tbxSearchPerson': '',
-			'ctl00$ContentPlaceHolder1$ddlYearSlut': '--',
-			'ctl00$ContentPlaceHolder1$btnSearch': '',
-			'ctl00$tbxOverlaySearch': '',
-			'ctl00$tbxOverlaySearchLangId': '',
-			'ctl00$ContentPlaceHolder1$ddlGridRows': '20',
-			'__VIEWSTATE': self.validation['__VIEWSTATE'],
-			'__VIEWSTATEGENERATOR': self.validation['__VIEWSTATEGENERATOR'],
-			'__EVENTVALIDATION': self.validation['__EVENTVALIDATION']
-		}
+        self.validation = validation
 
-	def _fetch_results(self):
-		r = self.session.post(PopScraper.URL, data=self.payload,
-				headers={'content-type': 'application/x-www-form-urlencoded',
-						'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36'})
+    def fetch(self):
+        payload = {
+            'ctl00$ContentPlaceHolder1$tbxSearchArtist': self.artist,
+            'ctl00$ContentPlaceHolder1$ddlFormat': '0',
+            'ctl00$ContentPlaceHolder1$ddlYearStart': self.year_start,
+            'ctl00$ContentPlaceHolder1$ddlYearSlut': self.year_end,
+            'ctl00$ContentPlaceHolder1$btnSearch': '',
+            'ctl00$ContentPlaceHolder1$ddlGridRows': '50',
+            '__VIEWSTATE': self.validation['__VIEWSTATE'],
+            '__VIEWSTATEGENERATOR': self.validation['__VIEWSTATEGENERATOR'],
+            '__EVENTVALIDATION': self.validation['__EVENTVALIDATION']
+        }
 
-		self._pprint_request(r.request)
+        r = self.session.post(PopScraper.URL, data=payload,
+                headers={'Content-Type': 'application/x-www-form-urlencoded',
+                        'User-Agent': PopScraper.FAKE_AGENT})
 
-		self._set_validation(r)
-		self.results = bs(r.text)
+        self._set_validation(r)
+        self.results = bs(r.text)
+        summary = self.results.find('div', class_='summary')
+        self.count = re.search(r'\d+', summary.text).group()
+        self._save_results()
 
-	def _fetch_next(self):
-		payload = {
-			'ctl00$ContentPlaceHolder1$smWebbEdit':'ctl00$ContentPlaceHolder1$upResult|ctl00$ContentPlaceHolder1$gvResult',
-			'__EVENTTARGET':'ctl00$ContentPlaceHolder1$gvResult',
-			'__EVENTARGUMENT':'Page$2',
-			'__LASTFOCUS':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchAlbum':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchArtist':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchSkivbolag':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchDistributor':'',
-			'ctl00$ContentPlaceHolder1$ddlFormat':'0',
-			'ctl00$ContentPlaceHolder1$ddlYearStart':'2006',
-			'ctl00$ContentPlaceHolder1$tbxSearchNo':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchTrack':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchStudio':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchForlag':'',
-			'ctl00$ContentPlaceHolder1$tbxSearchPerson':'',
-			'ctl00$ContentPlaceHolder1$ddlYearSlut':'--',
-			'ctl00$ContentPlaceHolder1$ddlGridRows':'20',
-			'ctl00$tbxOverlaySearch':'',
-			'ctl00$tbxOverlaySearchLangId':'',
-			'__VIEWSTATE': self.validation['__VIEWSTATE'],
-			'__VIEWSTATEGENERATOR': self.validation['__VIEWSTATEGENERATOR'],
-			'__EVENTVALIDATION': self.validation['__EVENTVALIDATION'],
-			'__ASYNCPOST':'true'
-		}
+    def fetch_all(self):
+        self.fetch()
 
-		headers = {
-			'Accept':'*/*',
-			'Accept-Encoding':'gzip,deflate',
-			'Accept-Language':'sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4,it;q=0.2',
-			'Cache-Control':'no-cache',
-			'Connection':'keep-alive',
-			# 'Content-Length':'22730',
-			'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-			'Host':'www.popfakta.se',
-			'Origin':'http://www.popfakta.se',
-			'Referer':'http://www.popfakta.se/sv/avancerad-sokning/',
-			'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36',
-			'X-MicrosoftAjax': 'Delta=true',
-			'X-Requested-With':'XMLHttpRequest'
-		}
+        page = 1
+        pages_count = math.ceil(int(self.count) / 50)
+        print self.count
+        print pages_count
+        while page <= pages_count:
+            page = page + 1
+            self._fetch_next(page=page)
+            self._save_results()
 
-		r = self.session.post(PopScraper.URL, data=payload,
-				headers=headers)
+    def _fetch_next(self, page='2'):
+        payload = {
+            'ctl00$ContentPlaceHolder1$smWebbEdit':'ctl00$ContentPlaceHolder1$upResult|ctl00$ContentPlaceHolder1$gvResult',
+            '__EVENTTARGET':'ctl00$ContentPlaceHolder1$gvResult',
+            '__EVENTARGUMENT':'Page$%s' % page,
+            'ctl00$ContentPlaceHolder1$tbxSearchArtist': self.artist,
+            'ctl00$ContentPlaceHolder1$ddlFormat':'0',
+            'ctl00$ContentPlaceHolder1$ddlYearStart': self.year_start,
+            'ctl00$ContentPlaceHolder1$ddlYearSlut': self.year_end,
+            'ctl00$ContentPlaceHolder1$ddlGridRows':'50',
+            '__VIEWSTATE': self.validation['__VIEWSTATE'],
+            '__VIEWSTATEGENERATOR': self.validation['__VIEWSTATEGENERATOR'],
+            '__EVENTVALIDATION': self.validation['__EVENTVALIDATION']
+        }
 
-		print '\n NEXT REQUEST COMING------>'
-		self._pprint_request(r.request)
+        r = self.session.post(PopScraper.URL, data=payload,
+                headers={'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+                        'User-Agent': PopScraper.FAKE_AGENT})
 
-		soup = bs(r.text).find('tr', class_='resultRow')
-		print '\n------> First row from next search result...'
-		print soup
+        self.results = bs(r.text)
+        self._set_validation(r)
 
-	def save_results(self, filename):
-		with open(filename, 'w') as f:
-			for row in self.results.find_all('tr', class_=['resultRow', 'resultRowAlt']):
-				record = ''
-				for cell in row.find_all('td'):
-					if cell.find('a'):
-						record += '"www.popfakta.se%s",' % cell.find('a')['href']
-					else:
-						record += '"%s",' % cell.text
-				f.write('%s\n' % record[:-1].encode('utf-8'))
+    def _save_results(self):
+        with open(self.filename, 'a+') as f:
+            for row in self.results.find_all('tr', class_=['resultRow', 'resultRowAlt']):
+                record = ''
+                for cell in row.find_all('td'):
+                    if cell.find('a'):
+                        record += '"www.popfakta.se%s",' % cell.find('a')['href']
+                    else:
+                        record += '"%s",' % cell.text
+                f.write('%s\n' % record[:-1].encode('utf-8'))
 
-
-
-	def _pprint_request(self, req):
-		print '%s %s' % (req.method, req.url)
-		for k, v in req.headers.items():
-			print '%s: %s' % (k, v)
-
-		print ''
-
-		for param in req.body.split('&'):
-			if len(param) > 60:
-				print param[0:60].replace('=',': ') + '...'
-			else:
-				print param.replace('=',': ')
 
 if __name__ == '__main__':
-	scraper = PopScraper(year_start='2006')
-	scraper.save_results(filename='hakan.csv')
-	scraper._fetch_next()
+    scraper = PopScraper(year_start='2006')
+    scraper.fetch_all()
